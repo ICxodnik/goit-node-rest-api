85b818064403bee4080c5183400c67092c21f0e5
Natalia <icxodnik@gmail.com>
2024-08-11 19:09:09 +0200

    implement basic controllers

--- controllers/contactsControllers.js
+++ controllers/contactsControllers.js
@@ -1,11 +1,35 @@
-import contactsService from "../services/contactsServices.js";
+import { ContactService } from "../services/contactsServices.js";
+import { FileDbRepository } from "../repositories/fileDbRepository.js";
+import path from "path";
 
-export const getAllContacts = (req, res) => {};
+const repository = new FileDbRepository(path.resolve("db/contacts.json"));
+const service = new ContactService(repository);
 
-export const getOneContact = (req, res) => {};
+export const getAllContacts = async (req, res) => {
+    res.status(200);
+    res.json(await service.listContacts());
+};
 
-export const deleteContact = (req, res) => {};
+export const getOneContact = async (req, res) => {
+    res.status(200);
+    const { id } = req.params;
+    res.json(await service.getContactById(id));
+};
 
-export const createContact = (req, res) => {};
+export const deleteContact = async (req, res) => {
+    res.status(200);
+    const { id } = req.params;
+    res.json(await service.removeContact(id));
+};
 
-export const updateContact = (req, res) => {};
+export const createContact = async (req, res) => {
+    res.status(200);
+    res.json(await service.addContact(req.body));
+};
+
+export const updateContact = async (req, res) => {
+    res.status(200);
+    const { id } = req.params;
+    res.json(await service.updateContact(id, req.body));
+};
+
