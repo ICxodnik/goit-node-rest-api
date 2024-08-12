60d700c597bc8afbd9e83d780ea1c3c0a43fee1a
Natalia <icxodnik@gmail.com>
2024-08-12 17:16:24 +0200

    add schemas

--- schemas/contactsSchemas.js
+++ schemas/contactsSchemas.js
@@ -1,9 +1,14 @@
 import Joi from "joi";
 
 export const createContactSchema = Joi.object({
-
+    name: Joi.string().max(100).required(),
+    email: Joi.email().max(100).required(),
+    phone: Joi.number().length(10).required(),
 })
 
 export const updateContactSchema = Joi.object({
-
+    id: Joi.string().required(),
+    name: Joi.string().max(100),
+    email: Joi.email().max(100),
+    phone: Joi.number().length(10),
 })
