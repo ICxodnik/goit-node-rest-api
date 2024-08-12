745fe517b538059af35d2f670a7ce2a99d46f2dd
Natalia <icxodnik@gmail.com>
2024-08-12 17:35:15 +0200

    add ctrlWrapper

--- controllers/contactsControllers.js
+++ controllers/contactsControllers.js
@@ -1,35 +1,44 @@
 import { ContactService } from "../services/contactsServices.js";
 import { FileDbRepository } from "../repositories/fileDbRepository.js";
+import { ctrlWrapper } from "../helpers/ctrlWrapper.js"
 import path from "path";
 
 const repository = new FileDbRepository(path.resolve("db/contacts.json"));
 const service = new ContactService(repository);
 
-export const getAllContacts = async (req, res) => {
+export async function getAllContacts(req, res) {
     res.status(200);
     res.json(await service.listContacts());
 };
 
-export const getOneContact = async (req, res) => {
+export async function getOneContact(req, res) {
     res.status(200);
     const { id } = req.params;
     res.json(await service.getContactById(id));
 };
 
-export const deleteContact = async (req, res) => {
+export async function deleteContact(req, res) {
     res.status(200);
     const { id } = req.params;
     res.json(await service.removeContact(id));
 };
 
-export const createContact = async (req, res) => {
+export async function createContact(req, res) {
     res.status(200);
     res.json(await service.addContact(req.body));
 };
 
-export const updateContact = async (req, res) => {
+export async function updateContact(req, res) {
     res.status(200);
     const { id } = req.params;
     res.json(await service.updateContact(id, req.body));
+};
+
+export default {
+    getAllContacts: ctrlWrapper(getAllContacts),
+    getOneContact: ctrlWrapper(getOneContact),
+    deleteContact: ctrlWrapper(deleteContact),
+    createContact: ctrlWrapper(createContact),
+    updateContact: ctrlWrapper(updateContact),
 };
 
