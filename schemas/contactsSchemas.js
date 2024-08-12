b0ac37c34a7a8d258232821a8940a9c1ff38412f
Natalia <icxodnik@gmail.com>
2024-08-12 18:23:46 +0200

    improve schemas

--- schemas/contactsSchemas.js
+++ schemas/contactsSchemas.js
@@ -1,14 +1,14 @@
 import Joi from "joi";
 
 export const createContactSchema = Joi.object({
-    name: Joi.string().max(100).required(),
-    email: Joi.email().max(100).required(),
-    phone: Joi.number().length(10).required(),
+    name: Joi.string().min(3).max(100).required(),
+    email: Joi.string().email().max(100).required(),
+    phone: Joi.string().length(10).pattern(/^[0-9]+$/).required(),
 })
 
 export const updateContactSchema = Joi.object({
-    id: Joi.string().required(),
-    name: Joi.string().max(100),
-    email: Joi.email().max(100),
-    phone: Joi.number().length(10),
+    id: Joi.string().min(20).required(),
+    name: Joi.string().min(3).max(100),
+    email: Joi.string().email().max(100),
+    phone: Joi.string().length(10).pattern(/^[0-9]+$/),
 })
