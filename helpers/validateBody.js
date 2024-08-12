245936627eb2b3dd3060ffa5a1d69b7ef58f6f8b
Natalia <icxodnik@gmail.com>
2024-08-12 19:31:46 +0200

    add validation for empty body

--- helpers/validateBody.js
+++ helpers/validateBody.js
@@ -2,7 +2,7 @@ import HttpError from "./HttpError.js";
 
 const validateBody = (schema) => {
   const func = (req, _, next) => {
-    const { error, value } = schema.validate(req.body);
+    const { error, value } = schema.validate(req.body, { allowUnknown: true, stripUnknown: true });
     if (error) {
       return next(HttpError(400, error.message));
     }
--- schemas/contactsSchemas.js
+++ schemas/contactsSchemas.js
@@ -7,8 +7,11 @@ export const createContactSchema = Joi.object({
 })
 
 export const updateContactSchema = Joi.object({
-    id: Joi.string().min(20).required(),
     name: Joi.string().min(3).max(100),
     email: Joi.string().email().max(100),
     phone: Joi.string().length(10).pattern(/^[0-9]+$/),
 })
+    .or("name", "email", "phone")
+    .messages({
+        "object.missing": "Body must have at least one field"
+    });
