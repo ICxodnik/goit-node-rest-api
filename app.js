de269efa4baa1579a0792b8c3e15b8e1ac7af9aa
Natalia <icxodnik@gmail.com>
2024-08-23 11:56:37 +0200

    move connection opening to app file

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 import express from "express";
 import morgan from "morgan";
 import cors from "cors";
-
 import contactsRouter from "./routes/contactsRouter.js";
+import db from "./db/index.js";
+import "dotenv/config";
 
 const app = express();
 
@@ -13,14 +14,24 @@ app.use(express.json());
 app.use("/api/contacts", contactsRouter);
 
 app.use((_, res) => {
-  res.status(404).json({ message: "Route not found" });
+    res.status(404).json({ message: "Route not found" });
 });
 
 app.use((err, req, res, next) => {
-  const { status = 500, message = "Server error" } = err;
-  res.status(status).json({ message });
+    const { status = 500, message = "Server error" } = err;
+    res.status(status).json({ message });
 });
 
 app.listen(3000, () => {
-  console.log("Server is running. Use our API on port: 3000");
+    console.log("Server is running. Use our API on port: 3000");
 });
+
+db.authenticate()
+    .then(() => {
+        console.log("Database connection successful");
+    })
+    .catch((error) => {
+        console.error("Unable to connect to the database", error);
+
+        process.exit(1);
+    });
--- db/index.js
+++ db/index.js
@@ -13,15 +13,4 @@ const sequelize = new Sequelize({
     },
 });
 
-sequelize
-    .authenticate()
-    .then(() => {
-        console.log("Database connection successful");
-    })
-    .catch((error) => {
-        console.error("Unable to connect to the database", error);
-
-        process.exit(1);
-    });
-
 export default sequelize;
