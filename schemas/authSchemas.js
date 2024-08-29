7d8077554417a2e4cc544c06a05bf963f7fe4ca9
Natalia <icxodnik@gmail.com>
2024-08-29 02:51:04 +0200

    add subscriptionSchema

--- schemas/authSchemas.js
+++ schemas/authSchemas.js
@@ -4,3 +4,7 @@ export const authSchema = Joi.object({
     email: Joi.string().email().max(100).required(),
     password: Joi.string().max(6).max(20).required(),
 });
+
+export const subscriptionSchema = Joi.object({
+    subscription: Joi.string().valid("starter", "pro", "business").required(),
+});
