b063a6aa0a2a55df662d0010f7dc4300b2569c43
Natalia <icxodnik@gmail.com>
2024-08-29 22:09:09 +0200

    add file Loader middleware

---
+++ middleware/fileLoader.js
@@ -0,0 +1,34 @@
+import multer from "multer";
+import path from "node:path";
+import { ApiError } from "../errors/apiError.js";
+
+const destination = path.resolve("temp");
+
+const storage = multer.diskStorage({
+    destination,
+    filename: (req, file, callback) => {
+        const uniquePrefix = `${Date.now()}`;
+        const filename = `${req.path}_${uniquePrefix}_${file.originalname}`;
+        callback(null, filename);
+    },
+});
+
+const limits = {
+    fileSize: 1024 * 1024 * 10,
+};
+
+const fileFilter = (req, file, callback) => {
+    const extension = file.originalname.split(".").pop();
+    if (extension === "exe") {
+        return callback(new ApiError(400, ".exe not allow extension"));
+    }
+    callback(null, true);
+};
+
+const upload = multer({
+    storage,
+    limits,
+    fileFilter,
+});
+
+export default upload;
